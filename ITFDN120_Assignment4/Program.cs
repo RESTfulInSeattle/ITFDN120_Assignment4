82f47295aa822bcaffac0118120d193a9ffc9347
David Clayton <david@davidclayton.me>
2017-08-03 18:55:06 -0700

    Implemented Solution

--- ITFDN120_Assignment4/Program.cs
+++ ITFDN120_Assignment4/Program.cs
@@ -8,8 +8,35 @@ namespace ITFDN120_Assignment4
 {
     class Program
     {
+       static bool IsInArray(int startElement, int endElement, int elementToFind, int[] elements)
+        {
+            int index = (endElement + startElement) / 2;
+
+            if (elements[index] == elementToFind)
+            {
+                return true;
+            }
+
+            //Go to the right
+            if (elementToFind > elements[index])
+            {
+                return IsInArray(index + 1, endElement, elementToFind, elements);
+            }
+
+            //Go to the left
+            if (elementToFind < elements[index])
+            {
+                return IsInArray(startElement, index - 1, elementToFind, elements);
+            }
+
+            return false;
+        }
         static void Main(string[] args)
         {
+            int[] numbers = { 2, 8, 22, 44, 56, 65, 72, 101, 208, 452, 801 };
+
+            Console.WriteLine(IsInArray(0, numbers.Length - 1, 22, numbers).ToString());
+            Console.ReadLine();
         }
     }
 }
